8e3b1185f28777acfba35657c5d89e87f5500d53
AdamsAdinatu <adinatu23@gmail.com>
2022-08-23 16:56:13 +0000

    routing

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Header from '../component/Header'
 function AboutScreen() {
   return (
     <div>
-      ABout Screen
+      <Header/>c
+      <p><h1>this my app is about countries, capital, currency,and their flag </h1></p>
     </div>
   )
 }
